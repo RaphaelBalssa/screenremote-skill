bdbdd4e6713286da6a557cd959c42102fb33971a
balssar <Raphael.balssa@etu.univ-grenoble-alpes.fr>
2019-08-05 09:27:25 +0200

    intialisation du skill

--- __init__.py
+++ __init__.py
@@ -1,14 +1,21 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import requests
 
 class Screenremote(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super().__init__("Screenremote")
 
-    @intent_file_handler('screenremote.intent')
-    def handle_screenremote(self, message):
-        self.speak_dialog('screenremote')
+    @intent_handler(IntentBuilder("").require("Direction").optionally("Query"
+        ).build())
+    def handle_direction_remote(self, message):
+        
+        self.speak_dialog('Confirm')
 
+    @intent_handler(IntentBuilder("").require("Key").optionally("Query"
+        ).build())
+    def handle_key_remote(self, message):
+        
+        self.speak_dialog('Confirm')
 
 def create_skill():
     return Screenremote()
