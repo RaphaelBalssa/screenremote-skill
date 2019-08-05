f7a5ffc55fac3647a681885dd084fb1ef6c127ec
balssar <Raphael.balssa@etu.univ-grenoble-alpes.fr>
2019-08-05 16:22:59 +0200

    configuration d'intents

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ import mycroft.audio
 
 import requests
 
-localhost = "http://localhost/testSkill.php"
+domainServer = "http://www.mytestskill.com"
 
 class Screenremote(MycroftSkill):
     def __init__(self):
@@ -22,7 +22,7 @@ class Screenremote(MycroftSkill):
     def handle_direction_remote(self, message):
         try:
             key_entry = message.data['Direction']
-            r = requests.post(localhost, data = key_entry)
+            r = requests.post(domainServer, data = key_entry)
             self.speak_dialog('Confirm')
 
     @intent_handler(IntentBuilder("").require("Key").optionally("Query"
@@ -30,24 +30,41 @@ class Screenremote(MycroftSkill):
     def handle_key_remote(self, message):
         try:
             data = {"user": user, "key_entry": message.data['Key']}
-            r = requests.post(localhost, data = key_entry)
-        self.speak_dialog('Confirm')
+            r = requests.post(domainServer, data = key_entry)
+            self.speak_dialog('Confirm')
 
     @intent_handler(IntentBuilder("").require("MenuQuery").optionally("TVButton"
         ).build())
     def handle_TV_menu(self, message):
-    
+        try:
+            data = { "key_entry": message.data['']}
+
+
     @intent_handler(IntentBuilder("").require("MenuQuery").optionally("RadioButton"
         ).build())
     def handle_radio_menu(self, message):
+        try:
+            data = {key_entry: message.data['MenuQuery']}
+
+            self.speak_dialog('Confirm')
 
     @intent_handler(IntentBuilder("").require("MenuQuery").optionally("GameButton"
         ).build())
     def handle_game_menu(self, message):
+        try:
+
 
     @intent_handler(IntentBuilder("").require("MenuQuery").optionally("Button"
         ).build())
     def handle_main_menu(self, message):
+        try:
+
+    @intent_handler(IntentBuilder("").require("Press").optionally("Query"
+        ).build())
+    def handle_press_enter(self, message):
+        try:
+            data = {"data": message.data['Press']}
+            requests.post(domainServer, data = data)
 
 def create_skill():
     return Screenremote()
