34abcae18171c54668b97964d781f6d7f258061c
balssar <Raphael.balssa@etu.univ-grenoble-alpes.fr>
2019-08-09 16:10:56 +0200

    complétion des méthodes de requetes http du skill

--- __init__.py
+++ __init__.py
@@ -9,6 +9,7 @@ from mycroft.util.format import nice_number
 from adapt.intent import IntentBuilder
 from mycroft.util.log import getLogger
 import mycroft.audio
+import random
 
 import requests
 
@@ -18,8 +19,9 @@ LOGGER = getLogger(__name__)
 # j'ai noté ici l'adresse à laquelle on fait la requête
 # mais c'était seulement pour le phase de test, le nom de domaine doit 
 # être paramétré dans un fichier JSON
-# domainServer = "http://www.mytestskill.com"
-
+IPaddress = ""
+port = "1966"
+domainName = "localhost"
 class Screenremote(MycroftSkill):
 
     # initialisation du Skill, concrètement on ne fait qu'initialiser son
@@ -29,99 +31,70 @@ class Screenremote(MycroftSkill):
 
     # Définition de l'intent directionnel, concrètement
     # permettre au curseur de monter quand on dit "monte"
-    #@intent_handler(IntentBuilder("").require('Go').require("Direction").optionally("Query"
-    #    ).build())
-    #def handle_direction_remote_intent(self, message):
-    #    try:
-            #key_input = message.data['Direction']
-            #r = requests.post(domainServer, data = key_input)
-    #        self.speak_dialog('Confirm')
-
-    #    except Exception as e:
-    #        self.speak_dialog('connection.error')
+    @intent_handler(IntentBuilder("").require('Go').require("Direction").optionally("Query"))
+    def handle_direction_remote_intent(self, message):
+       try:
+            key_input = "KEY_" + message.data['Direction']
+            key_input = key_input.upper()
+            sendRequest("remote", key_input)
             
-    @intent_file_handler('direction.press.intent')
-    def handle_direction_press_f(self, message):
-        self.handle_direction_press(message)   
-
-    # Handle: direction pressed
-    def handle_direction_press(self, message):
-
-        try:
             self.speak_dialog('Confirm')
 
-        except Exception as e:
-            self.speak_dialog('connection.error')    
+       except Exception as e:
+            self.speak_dialog('connection.error')
+            
+    # @intent_file_handler('direction.press.intent')
+    # def handle_direction_press_f(self, message):
+    #     self.handle_direction_press(message)   
+
+    # # Handle: direction pressed
+    # def handle_direction_press(self, message):
+
+    #     try:
+    #         r = requests.post(domainServer, data = 'my_key_input')
+    #         self.speak_dialog('Confirm')
+
+    #     except Exception as e:
+    #         self.speak_dialog('connection.error')    
 
 
     # Définition de l'intent des boutons, concrètement
     # appuie sur le bouton vert ou encore le bouton 1 
     # si on lui dit de le faire
-    #@intent_handler(IntentBuilder("").require('Press').require("Key"
-    #    ).optionally("Query").build())
-    #def handle_key_remote_intent(self, message):
-    #    try:
-            #data = {"user": user, "key_input": message.data['Key']}
-            #r = requests.post(domainServer, data = key_input)
-    #        self.speak_dialog('Confirm')
-        
-    #    except Exception as e:
-    #        self.speak_dialog('connection.error')
-
-    @intent_file_handler('button.press.intent')
-    def handle_button_press_f(self, message):
-        self.handle_button_press(message)   
-
-    # Handle: button pressed
-    def handle_button_press(self, message):
-
+    @intent_handler(IntentBuilder("").require('Press').require("Order").optionally("Query"))
+    def handle_key_remote_intent(self, message):
         try:
+            key_input = message.data['Order']
+            sendRequest("order", key_input)
             self.speak_dialog('Confirm')
-
+        
         except Exception as e:
-            self.speak_dialog('connection.error') 
+            self.speak_dialog('connection.error')
 
-    # @intent_handler(IntentBuilder("").require("MenuQuery").require("TVButton"
-    #     ).build())
-    # def handle_TV_menu(self, message):
+    # @intent_file_handler('button.press.intent')
+    # def handle_button_press_f(self, message):
+    #     self.handle_button_press(message)   
+
+    # # Handle: button pressed
+    # def handle_button_press(self, message):
+
     #     try:
-    #         data = { "key_input": message.data['']}
+    #         r = requests.post(domainServer, data = 'my_key_input')
     #         self.speak_dialog('Confirm')
 
     #     except Exception as e:
-    #         self.speak_dialog('connection.error')
+    #         self.speak_dialog('connection.error') 
 
+    def sendRequest(typeR, value):
+        try:
+            myrand=str(random.random())
+            URL = "http://"+domainName+"/wget.php?type="+typeR+"&value="+value+"&server="+IPaddress+port+"&nocache="+myrand+"&end"
+            r = requests.get(URL)
+            return r.status_code
 
-    # @intent_handler(IntentBuilder("").require("MenuQuery").require("RadioButton"
-    #     ).build())
-    # def handle_radio_menu(self, message):
-    #     try:
-    #         data = {key_input: message.data['MenuQuery']}
-            
-    #         self.speak_dialog('Confirm')
+        except Exception as e:
+            self.speak_dialog('connection.error')
 
-    # @intent_handler(IntentBuilder("").require("MenuQuery").require("GameButton"
-    #     ).build())
-    # def handle_game_menu(self, message):
-    #     try:
-            
-    #         self.speak_dialog('Confirm')
-
-    # @intent_handler(IntentBuilder("").require("MenuQuery").require("Button"
-    #     ).build())
-    # def handle_main_menu(self, message):
-    #     try:
-    #         self.speak_dialog('Confirm')
-
-    # méthode permettant d'appuyer sur le bouton OK
-    #def handle_press_input(self, message):
-    #    try:
-    #        data = {"data": message.data['Press']}
-    #        requests.post(domainServer, data = data)
-
-
-   # def sendRequest(url):
-   #    return False
 
 def create_skill():
     return Screenremote()
